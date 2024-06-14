20e421b1236cfaa275cbc1127b2539f7923044c6
Sparsh Sharma <142775479+itsparsh10@users.noreply.github.com>
2024-06-14 12:46:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import ToDoList from './components/ToDoList'
+
+
+function App() {
+
+
+  return (
+    <>
+      <ToDoList />
+      
+      
+    </>
+  )
+}
+
+export default App
